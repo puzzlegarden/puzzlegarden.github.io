620858d19a8d24362dba861b9f45d92360a4e79f
Jong Hoon Kim <zplanet@hotmail.com>
2025-07-01 15:46:44 -0400

    add Kozzle page

---
+++ assets/js/5e95c892.9dce289f.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkpuzzle_garden_website=self.webpackChunkpuzzle_garden_website||[]).push([[9647],{7121:(e,s,r)=>{r.r(s),r.d(s,{default:()=>d});r(6540);var u=r(4164),a=r(5500),n=r(7559),t=r(2831),c=r(1656),l=r(4848);function d(e){return(0,l.jsx)(a.e3,{className:(0,u.A)(n.G.wrapper.docsPages),children:(0,l.jsx)(c.A,{children:(0,t.v)(e.route.routes)})})}}}]);
